387ee32dc8751f6500dc26197e1264f6fa38560b
Margarita45 <smrp21082004@gmail.com>
2023-03-11 08:04:04 -0500

    Navbar

--- app/Views/principal/header.php
+++ app/Views/principal/header.php
@@ -3,10 +3,16 @@
 
 <head>
     <title>Cuentas Claras</title>
+    <link rel="icon" type="ima  ge/svg+xml" href="<?php echo base_url() ?>/favicon.ico" />
     <meta charset="utf-8" />
     <link rel="stylesheet" href="<?php echo base_url() ?>/css/header.css">
-    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+    rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+    crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
+     <link rel="stylesheet" href="<?php echo base_url() ?>/bootstrap/bootstrap.min.css">
+     <script src="<?php echo base_url() ?>/bootstrap/bootstrap.bundle.min.js"></script>
 </head>
 
 <header class="containerH">
@@ -14,10 +20,56 @@
 
     <div class="titulo">
         <h1 style="text-align:center"><?php echo $titulo; ?></h1>
-        <h2 style="text-align:center "><?php echo $nombre; ?></h2>   
+        <h6 style="text-align:center "><?php echo $nombre; ?></h2>   
     </div>
-
-        <img src="<?php echo base_url(); ?>img/sena.png" alt="">
+        <a href="https://oferta.senasofiaplus.edu.co/sofia-oferta/" target="_blank" class="logosena">
+            <img src="<?php echo base_url(); ?>img/sena.png" alt="">
+        </a>
+        
 </header>
 
+
+
+<!-- Barra de Navegacion -->
+<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <div class="container-fluid">
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 text-light">
+      <!-- <li class="nav-item">
+          <a class="nav-link text-light" href="#">Paises</a>
+        </li>
+      <li class="nav-item">
+          <a class="nav-link text-light" href="#">Municipios</a>
+        </li>
+      <li class="nav-item">
+          <a class="nav-link text-light" href="#">Departamentos</a>
+        </li>
+      <li class="nav-item">
+          <a class="nav-link text-light" href="#">Empleados</a>
+        </li>
+      <li class="nav-item">
+          <a class="nav-link text-light disable" href="#">Cargos</a>
+        </li> -->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Empleados
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item disabled" href="#">Cargos</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-light" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Paises
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Departamentos</a></li>
+            <li><a class="dropdown-item" href="#">Municipios</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
